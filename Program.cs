ab129e333f16cfc350710091751ad06d911e69ce
Roman Annenkov <roman.annenkov@gmail.com>
2023-02-13 15:19:53 +0300

    create method PrintArray

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/* Task: Write a program that from an array of strings forms an array of strings with a length of no more than 3 characters.
+1. Create method for print arrays.
+2. Create method to generate result array.
+3. Create start array and fill it with strings.
+4. Create variable for result array and call a method(from step 2) and pass an array(from step 3) to it.
+5. Call a method(from step 1) and pass an array(from step 4) to it.
+*/
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == arr.Length - 1)
+        {
+            Console.Write($"{arr[i]}");
+            break;
+        }
+        Console.Write($"{arr[i]}, ");
+    }
+
+    Console.Write("]");
+}
